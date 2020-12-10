c3b58ee3d91b88bda902cbe11db56008479954b1
GaudiestTooth17 <alex.r.anthon@gmail.com>
2020-12-10 09:42:10 -0700

    Removed print_if

--- TSPSolver.py
+++ TSPSolver.py
@@ -311,8 +311,3 @@ def incrementPheromoneMatrix(pheromoneMatrix: np.ndarray, route, cost):
     increaseVal = (5*len(route)) / cost
     for i in range(len(route) - 1):
         pheromoneMatrix[route[i]][route[i + 1]] += increaseVal
-
-
-def print_if(msg: str, cond: bool, *, end=None) -> None:
-    if cond:
-        print(msg, end=end)
